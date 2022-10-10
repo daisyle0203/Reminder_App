d1d4de5ad131e99e9b30cf529720c77289965da3
Daisy Le <daisy.le0203@gmail.com>
2022-10-10 00:27:28 -0500

    added button style and text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types"
 import Button from "./Button"
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   const onClick = () => {
     console.log("Click")
   }
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="steelblue" text="Add" onClick={onClick} />
+      <Button
+        color={showAdd ? "red" : "steelblue"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   )
 }
