cdec0816fdeafc79044eb1cda9a2d7c3b1228c98
Daisy Le <daisy.le0203@gmail.com>
2022-12-04 21:39:53 -0600

    update router v6 and add TaskDetails component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 import About from "./components/About"
+import TaskDetails from "./components/TaskDetails"
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -94,25 +95,27 @@ const App = () => {
           onAdd={() => setShowAddTask(!showAddTask)}
           showAdd={showAddTask}
         />
-        <Route
-          path="/"
-          exact
-          render={(props) => (
-            <>
-              {showAddTask && <AddTask onAdd={addTask} />}
-              {tasks.length > 0 ? (
-                <Tasks
-                  tasks={tasks}
-                  onDelete={deleteTask}
-                  onToggle={toggleReminder}
-                />
-              ) : (
-                "No Tasks To Show"
-              )}
-            </>
-          )}
-        />
-        <Route path="/about" component={About} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/tasks/:id" element={<TaskDetails />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
